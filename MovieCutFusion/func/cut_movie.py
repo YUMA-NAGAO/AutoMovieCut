e4f0de06fd0a05e27949fb0c6117ae2da426772f
YUMA-NAGAO <55423883+YUMA-NAGAO@users.noreply.github.com>
2021-02-09 11:24:56 +0900

    [edit]動画の無音部分のカットから合成までを自動化した

--- MovieCutFusion/func/cut_movie.py
+++ MovieCutFusion/func/cut_movie.py
@@ -1,7 +1,7 @@
 import time
 import os
 import subprocess
-
+import natsort
 
 
 def get_movie(wk_dir):
@@ -29,9 +29,6 @@ def cut_silent(movie1, dB1):
                     time_list.append(float(words1[i + 1]))
     silence_section_list = list(zip(*[iter(time_list)] * 2))
     movie_name = movie1.split(".")
-    """if str(silence_section_list[0][0]) != "0.0":
-        split_file1 = "../cut/" + movie_name[0] + "_0" + ".mp4"
-        subprocess.run(["ffmpeg", "-ss", str(0), "-i", movie1, "-t", str(silence_section_list[1][0]), split_file1], stdout=subprocess.PIPE, stderr=subprocess.PIPE)"""
 
     for i in range(len(silence_section_list) - 1):
         split_file = "../cut/" + movie_name[0] + "_" + str(i + 1) + ".mp4"
@@ -39,12 +36,22 @@ def cut_silent(movie1, dB1):
         print(movie_name[0] + "_" + str(i + 1) + ".mp4")
 
 
+def merge_movie(movie_list1):
+    os.chdir("../cut")
+    FileName="out.mp4"
+    movie_list1 = [ m for m in movie_list1]
+    with open("tmp.txt", "w") as fp:
+        lines = [f"file '{line1}'" for line1 in movie_list1]
+        fp.write("\n".join(lines))
+    subprocess.run(["ffmpeg", "-f", "concat", "-safe", "0", "-i", "tmp.txt", "-c", "copy",FileName ], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    subprocess.run(["mv","out.mp4","../merged"])
+
 
 if __name__ == "__main__":
-    subprocess.run(["mkdir","../cut"])
-
-    movie_list = get_movie("../input")
     start =time.time()
+    subprocess.run(["mkdir","../cut"])
+    movie_list = get_movie("../input")
+    
     for movie in movie_list:
         print("処理する動画")
         print(movie)
@@ -69,7 +76,12 @@ if __name__ == "__main__":
                 if float(word) < 0.5:
                     os.remove(movie)
 
-    ProcessTime=time.time()-start
 
-    print(ProcessTime)
+    merge_movie(movie_list)
+
+    ProcessTime=time.time()-start
+    minutes=str(ProcessTime//60)
+    second=str(ProcessTime%60)
+
+    print(minutes+"分"+second+"秒")
 
